eb3afb8f3daa9f7cbadcc0dbb67691c500742016
carlosae06 <carlosaugusto.ae060897@gmail.com>
2022-10-03 13:28:18 -0400

    Peliculas añadido

--- FrontEnd/DTOs/Categoria/CategoriaDTO.cs
+++ FrontEnd/DTOs/Categoria/CategoriaDTO.cs
@@ -6,7 +6,9 @@ namespace FrontEnd.DTOs.Categoria
     {
     
         public int Id { get; set; }
-    
+
+        [Required(ErrorMessage = "El campo nombre es obligatorio.")]
+        [StringLength(50)]
         public string Nombre { get; set; }
 
         public bool Estado { get; set; }
